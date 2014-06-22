e767a34fcaa56bf97d01f29add779bc1126b2213
Nathan Owens <nathan_owens@cable.comcast.com>
2014-06-22 10:00:55 -0400

    tweak example

--- examples/walkexample.go
+++ examples/walkexample.go
@@ -11,14 +11,14 @@ import (
 	"path/filepath"
 	"time"
 
-	"github.com/virtuallynathan/gosnmp"
+	g "github.com/virtuallynathan/gosnmp"
 )
 
 func usage() {
-	fmt.Println("\nUsage:")
-	fmt.Printf("   %s host [oid]\n", filepath.Base(os.Args[0]))
-	fmt.Println("     host - the host to walk/scan")
-	fmt.Println("     oid  - the MIB/Oid defining a subtree of values")
+	fmt.Println("Usage:")
+	fmt.Printf("%s host [oid]\n", filepath.Base(os.Args[0]))
+	fmt.Println("host - the host to walk/scan")
+	fmt.Println("oid  - the MIB/Oid defining a subtree of values")
 	os.Exit(1)
 }
 
@@ -32,30 +32,30 @@ func main() {
 		oid = os.Args[2]
 	}
 
-	gosnmp.Default.Target = target
-	gosnmp.Default.Timeout = time.Duration(10 * time.Second) // Timeout better suited to walking
-	err := gosnmp.Default.Connect()
+	g.Default.Target = target
+	g.Default.Timeout = time.Duration(10 * time.Second) // Timeout better suited to walking
+	err := g.Default.Connect()
 	if err != nil {
 		fmt.Printf("Connect err: %v\n", err)
 		os.Exit(1)
 	}
-	defer gosnmp.Default.Conn.Close()
+	defer g.Default.Conn.Close()
 
-	err = gosnmp.Default.BulkWalk(oid, printValue)
+	err = g.Default.BulkWalk(oid, printValue)
 	if err != nil {
 		fmt.Printf("Walk Error: %v\n", err)
 		os.Exit(1)
 	}
 }
 
-func printValue(pdu gosnmp.SnmpPDU) error {
+func printValue(pdu g.SNMPData) error {
 	fmt.Printf("%s = ", pdu.Name)
 
 	switch pdu.Type {
-	case gosnmp.OctetString:
+	case g.OctetString:
 		fmt.Printf("STRING: %s\n", pdu.Value.(string))
 	default:
-		fmt.Printf("TYPE %d: %d\n", pdu.Type, gosnmp.ToBigInt(pdu.Value))
+		fmt.Printf("TYPE %d: %d\n", pdu.Type, g.ToBigInt(pdu.Value))
 	}
 	return nil
 }
